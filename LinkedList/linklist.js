efb6378c9b868fc3ab0d296d1e673adceba8a542
Ascended300 <eduardofelix014@gmail.com>
2022-04-17 18:18:11 -0300

    Lógica de las listas doblemente enlazadas

--- LinkedList/linklist.js
+++ LinkedList/linklist.js
@@ -47,7 +47,7 @@ class MySinglyLinkedList {
             current.next = nodo
             this.tail = nodo
         } else {
-            this.append(value, current.next)
+            this.appendRecursive(value, current.next)
         }
     }
     prepend(value) {
@@ -57,7 +57,59 @@ class MySinglyLinkedList {
         this.head = newNode;
 
         this._length++;
+        return this;
+    }
+    insert(index, value) {
+        if (index >= this._length) {
+            return this.append(value);
+        }
+
+        const newNode = new Node(value);
+        const firstPointer = this.getTheIndex(index - 1);
+        const holdingPointer = firstPointer.next;
+
+        firstPointer.next = newNode;
+        newNode.next = holdingPointer;
+
+        this._length++;
+        return this;
+    }
+    getTheIndex(index) {
+        let counter = 0;
+        let currentNode = this.head;
+
+        while (counter !== index) {
+            currentNode = currentNode.next;
+            counter++;
+        }
+
+        return currentNode;
+    }
+    delete(index) {
+        if (index <= 0) return this.shift();
+        if (index >= this.length) return this.pop();
+        const prevDeletedNode = this.getTheIndex(index - 1);
+        prevDeletedNode.next = prevDeletedNode.next.next;
+        this.length--;
 
         return this;
     }
+
+    shift() {
+        const secondNode = this.head.next;
+        this.head = secondNode;
+        this.length--;
+
+        return this;
+    }
+
+    pop() {
+        const prevLastNode = this.getTheIndex(this.length - 2);
+        this.tail = prevLastNode;
+        prevLastNode.next = null;
+        this.length--;
+
+        return this;
+    }
+
 }
